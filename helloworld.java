b5c9f1f6d85b5c5de0377bed2404c17223ab04a5
samplearjun <32512651+samplearjun@users.noreply.github.com>
2018-02-20 14:39:57 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,28 +1,15 @@
-class Programming {
+
+class Programming {
   //constructor method
   Programming() {
-    System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
- System.out.println("Constructor method called.");
+    System.out.println("Hello World");
+ 
+ 
 
   }
-}public class helloworld{
+}
+public class helloworld
+{
   public static void main(String[] args) {
     Programming object = new Programming(); //creating object
   }
