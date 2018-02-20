7fa2e1cad254ee41b0eb1782e3c8eb921db3d26b
samplearjun <32512651+samplearjun@users.noreply.github.com>
2018-02-20 16:06:22 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,16 +1,7 @@
-
-class Programming {
-  //constructor method
-  Programming() {
-    System.out.println("Hello World Jenkins");
- 
- 
-
-  }
-}
 public class helloworld
 {
   public static void main(String[] args) {
-    Programming object = new Programming(); //creating object
+        System.out.println("Hello World Jenkins");
+ //creating object
   }
 }
