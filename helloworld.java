85fb20f8e5de9fca3a8aefb39b948cb44e9384c3
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2018-04-04 00:45:50 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Good Day");
  //creating object
   }
 }
