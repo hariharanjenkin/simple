4b169d556331f3febf12acb7428980a8121c5789
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-04-18 12:19:04 +0000

    Add files via upload

---
+++ tedt.py
@@ -0,0 +1,22 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
+
+client = boto3.client('ec2', region_name=region, aws_access_key_id=accesskey, aws_secret_access_key=secretkey)
+
+
+test1 = {}
+des_ins = client.describe_instances()
+
+for step1 in des_ins['Reservations']:
+	for step2 in step1['Instances']:
+		test1['InstancID'] = step2['InstanceId']
+		test1['Lanunch time'] = step2['LaunchTime']
+		print (test1)
+
+
+
+
