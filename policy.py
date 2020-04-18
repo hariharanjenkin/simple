51fa38c73137361e0b28fc0d5bf657f725f25c50
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-04-18 12:44:50 +0000

    Add files via upload

---
+++ policy.py
@@ -0,0 +1,42 @@
+import boto3
+from datetime import date, datetime, timedelta,timezone
+from datetime import date
+import csv
+
+client = boto3.client('iam')
+
+def accesskey_10days_data(writer):
+
+	accessskey10days_age = {}
+
+#get the list of users
+	listing_usr = client.list_users()
+	for usr_name in listing_usr['Users']:
+
+
+#get the Access_key details of users
+		accessky_data = client.list_access_keys(UserName=usr_name['UserName'])
+		for usr_data_frm_acckey in accessky_data['AccessKeyMetadata']:
+
+		
+#getting the current date using python datetime.now defuly function
+			current_date = datetime.now(timezone.utc)
+			age = (current_date-usr_data_frm_acckey['CreateDate']).days
+			if age >= 10:
+				accessskey10days_age['USERNAME'] = usr_data_frm_acckey['UserName']
+				accessskey10days_age['CURRENT_ACCESSKEY'] = usr_data_frm_acckey['AccessKeyId']
+				accessskey10days_age['AGE'] = usr_data_frm_acckey['CreateDate']
+				writer.writerow(accessskey10days_age)
+				print (accessskey10days_age)
+
+
+def main():
+	fieldnames = ['USERNAME','CURRENT_ACCESSKEY','AGE']
+	file_name = "accessskey10days_age.csv"
+	with open (file_name,"w",newline='') as csv_file:
+		writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+		writer.writeheader()
+		
+		accesskey_10days_data(writer)
+
+main()
