72d02a6bffba4e9b533d58f7018ad56e92b4985a
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-05-17 09:47:13 +0530

    Update Jenkins_pipeline_script.py

--- Jenkins_pipeline_script.py
+++ Jenkins_pipeline_script.py
@@ -1,7 +1,7 @@
 import boto3
 import sys
 from datetime import date
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, timezone
 import csv
 
 accesskey = sys.argv[1]
