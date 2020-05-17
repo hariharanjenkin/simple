8ca1f39ac089f72366c35b8dc64a3b5c42349f44
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-05-17 09:52:38 +0530

    Add files via upload

---
+++ ins-new.py
@@ -0,0 +1,14 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
+
+
+client = boto3.client('ec2',region_name=region, aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
+
+data1 = client.describe_instances()
+for data2 in data1["Reservations"]:
+	for data3 in data2["Instances"]:
+		print(data3["InstanceId"])
