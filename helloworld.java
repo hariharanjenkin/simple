130ca50fd744d9958205c7111d16fe2681277945
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-06-04 08:30:13 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hi-Tech Institution - Ph 7092 90 91 92");
+        System.out.println("Hari Haran");
  //creating object
   }
 }
