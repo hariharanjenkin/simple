759ac2a57c0bca3f4160480cb7d56f5aed758204
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-07-10 20:57:13 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Executed suucesfully - Hari");
+        System.out.println("Executed successfully.....!!!! - Hari");
  //creating object
   }
 }
