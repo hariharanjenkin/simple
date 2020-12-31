936031f476e2acd22658c2e23362c3c5aff21ed2
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-12-31 08:56:19 +0530

    Updated
    I just added the print statement

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Welcome to all, Have a good day....!!!!");
+        System.out.println("Welcome to all..!!!!");
  //creating object
   }
 }
