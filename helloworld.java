204357e4aceb8133cf98fb93b9bd420d90d9fef3
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2021-03-02 08:26:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("checking github webhobb");
+        System.out.println("checking githubnnnn webhobb");
  //creating object
   }
 }
