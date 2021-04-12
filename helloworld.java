34eebad29d6e87a15e86bb40c0536a3325d5c302
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2021-04-12 20:27:43 +0530

    updated the code
    testing update

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Welcome to Hi-Tech Jenkins Training");
+        System.out.println("Welcome to Hi-Tech Jenkins Training, Stay Safe");
  //creating object
   }
 }
