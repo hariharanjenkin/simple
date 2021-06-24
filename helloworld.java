3fa622cb1107aef4a502a47790b4d222faf2af7c
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2021-06-24 20:21:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Welcome to Hi-Tech Jenkins Training, Stay Safe...............");
+        System.out.println("Welcome to Hi-Tech Jenkins Training, Stay Safe.............");
  //creating object
   }
 }
